08c73e2f616165fa8b5d73e42dbbd7c11dd6ec51
umutaev <M8858110177@yandex.com>
2021-03-06 08:35:39 +0500

    Added rules decorator

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from vkapi import VKApiMethods
-from secrets import vk_api_key, vk_group_id
+from vkapi import VKApiMethods, VKApiLongPolling
+import secrets
+import time
+
+vk = VKApiMethods(secrets.vk_api_key, is_group=True)
+polling = VKApiLongPolling(vk)
+
+"""def test(rule, **options):
+    def decorator(f):
+        endpoint = options.pop("endpoint", None)
+        print(str(rule), str(f))
+        return f
+    return decorator
+
+@test('test-command')
+def test():
+    print("Test function")"""
+
+
+@polling.rule(r"[Бб][Оо][Тт],* *(?i)рецепт")
+def recipe():
+    print("User asked for recipe")
+
 
 if __name__ == "__main__":
-    vk = VKApiMethods(vk_api_key, is_group=True)
-    print(vk.get_long_poll_server(vk_group_id))
+    polling.start_polling(secrets.vk_group_id)
--- vkapi.py
+++ vkapi.py
@@ -1,5 +1,7 @@
 import requests
 import asyncio
+import re
+from typing import Callable
 
 
 class VKApiMethods:
@@ -24,26 +26,55 @@ class VKApiMethods:
 
 class VKApiLongPolling:
     polling = False
+    temp_list = []
+    group_id = 0
+    dispatching_array = {}
 
-    def __init__(self, vk_api_methods: VKApiMethods, update_ts=True, update_timeout=30):
+    def __init__(self, vk_api_methods: VKApiMethods, update_timeout=30):
         self.__vk_api_class = vk_api_methods
-        self.__update_ts = update_ts
         self.__update_timeout = update_timeout
         json = self.__vk_api_class.get_long_poll_server(202959880)
-        self.__vk_polling_server = f"{json['server']}?key={json['key']}&ts={json['ts']}&wait=30"
+        self.__ts = json['ts']
+        self.__vk_polling_server = f"{json['server']}?act=a_check&key={json['key']}&wait=30"
 
-    async def start_bot_polling(self, group_id):
+    async def __bot_polling(self):
         self.polling = True  # Theoretically, it is possible to stop polling by setting this to False from outside.
         while self.polling:
-            response = requests.get(self.__vk_polling_server).json()
+            response = requests.get(self.__vk_polling_server + '&ts=' + self.__ts).json()
             if 'failed' in response:
                 if response['failed'] == 2:
-                    json = self.__vk_api_class.get_long_poll_server(202959880)
-                    self.__vk_polling_server = f"{json['server']}?key={json['key']}&ts={json['ts']}&wait=30"
+                    json = self.__vk_api_class.get_long_poll_server(self.group_id)
+                    self.__vk_polling_server = f"{json['server']}?act=a_check&key={json['key']}&wait=30"
                     await asyncio.sleep(1)
                 # TODO: handle more errors
+            if 'updates' in response:
+                self.__ts = response['ts']
+                for item in response['updates']:
+                    if item['type'] == 'message_new' and not item['object']['message']['text'] == '':
+                        for i in self.dispatching_array.keys():
+                            if i.match(item['object']['message']['text']):
+                                self.dispatching_array[i]()
 
-            pass
+    def start_polling(self, group_id: int) -> None:
+        self.group_id = group_id
+        asyncio.run(self.__bot_polling())
+
+    def stop_polling(self) -> None:
+        self.polling = False
+
+    def add_to_dispatcher(self, regular_expression: str, function: Callable) -> None:
+        if regular_expression == "":
+            raise ValueError("Dispatching regular expression is empty.")
+        regular_pattern = re.compile(regular_expression)
+        if regular_pattern in self.dispatching_array:
+            raise ValueError("You have already set this regular expression.")
+        self.dispatching_array[regular_pattern] = function
+
+    def rule(self, regular_expression: str):
+        def decorator(f):
+            self.add_to_dispatcher(regular_expression, f)
+            return f
+        return decorator
 
 
 class VKApiErrors(BaseException):
